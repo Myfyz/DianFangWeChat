bfec174f7da6d77124ec0ca11be2215b5b33d3ca
FYZ <1790462158@qq.com>
2017-04-16 19:35:22 +0800

    创建了person类

---
+++ DFWeChat/DFWeChat/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  DFWeChat
+//
+//  Created by 麦子 on 2017/4/16.
+//  Copyright © 2017年 com.fujia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ DFWeChat/DFWeChat/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  DFWeChat
+//
+//  Created by 麦子 on 2017/4/16.
+//  Copyright © 2017年 com.fujia. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
